874680fe5c51ba9ca61b4a91dd1b139185fd41c4
StressoBoi <38673935+Taygh@users.noreply.github.com>
2020-05-14 16:19:15 -0700

    Spring par 2 electric boogaloo

---
+++ UnityProjectFolder/ProjectAtom/Assets/Scripts/Level/Platforms/Spring.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour
+{
+
+    [SerializeField]
+    float force;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, force, 0));
+    }
+}
