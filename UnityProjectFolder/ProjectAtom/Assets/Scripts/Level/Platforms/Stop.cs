64118710b004c18ad69497f0cf768b5599a728e6
StressoBoi <38673935+Taygh@users.noreply.github.com>
2020-05-18 20:55:36 -0700

    First version of Stop platform

---
+++ UnityProjectFolder/ProjectAtom/Assets/Scripts/Level/Platforms/Stop.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stop : MonoBehaviour
+{
+    [SerializeField] float timeToWait;
+    [SerializeField] FloatVariable speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        float temp = speed;
+        Debug.Log(temp);
+        speed.SetValue(0);
+        StartCoroutine(WaitAndRelease(timeToWait, temp));
+        Debug.Log("hit");
+    }
+
+    IEnumerator WaitAndRelease(float time, float originalSpeed)
+    {
+        yield return new WaitForSeconds(time);
+        speed.SetValue(originalSpeed);
+    }
+}
