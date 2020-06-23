1a53625001469d7559a4ba0433c8c283d36816d3
Aidan Takami <aidan.takami@yahoo.com>
2020-06-23 15:45:42 -0700

    Added Main Menu and some Sound FX

---
+++ UnityProjectFolder/ProjectAtom/Assets/MainMenuScript.cs
@@ -0,0 +1,419 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    [SerializeField] public GameObject MainMenuGO;
+    [SerializeField] public GameObject OptionsMenuGO;
+    [SerializeField] public GameObject ScoresMenuGO;
+
+    [SerializeField] public AudioSource menuNextSelectionSound;
+    private bool joystick1ReturnToZero = true;
+    private bool joystick2ReturnToZero = true;
+
+    private bool mainMenuActive = true;
+    private bool optionsMenuActive = false;
+    private bool scoresMenuActive = false;
+
+    private int selectedButton = 0;
+    [SerializeField] private List<Button> mainMenuButtons = new List<Button> ();
+    [SerializeField] private List<Button> optionsMenuButtons = new List<Button> ();
+    [SerializeField] private List<Button> scoresMenuButtons = new List<Button> ();
+
+    // Start is called before the first frame update
+    void Start ()
+    {
+        ScoresMenuGO.SetActive (false);
+        OptionsMenuGO.SetActive (false);
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        //Main Menu Navigation
+        if (mainMenuActive)
+        {
+            if ((Input.GetAxis ("P1Left Stick Vertical") < -0.8f && joystick1ReturnToZero))
+            {
+                //Change Selection
+                if (selectedButton == 0)
+                {
+                    mainMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton = mainMenuButtons.Count - 1;
+                }
+                else
+                {
+                    mainMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton--;
+                }
+
+                menuNextSelectionSound.Play ();
+                joystick1ReturnToZero = false;
+
+            }
+
+            else if ((Input.GetAxis ("P2Left Stick Vertical") < -0.8f && joystick1ReturnToZero))
+            {
+                //Change Selection
+                if (selectedButton == 0)
+                {
+                    mainMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton = mainMenuButtons.Count - 1;
+                }
+                else
+                {
+                    mainMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton--;
+                }
+
+                menuNextSelectionSound.Play ();
+                joystick2ReturnToZero = false;
+
+            }
+
+            else if ((Input.GetAxis ("P1Left Stick Vertical") > 0.8f) && joystick1ReturnToZero)
+            {
+                if (selectedButton == mainMenuButtons.Count - 1)
+                {
+                    mainMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton = 0;
+                }
+                else
+                {
+                    mainMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton++;
+                }
+
+                menuNextSelectionSound.Play ();
+
+                //bool to make player bring stick back to 0
+                joystick1ReturnToZero = false;
+
+            }
+            else if (Input.GetAxis ("P2Left Stick Vertical") > 0.8f && joystick2ReturnToZero)
+            {
+                if (selectedButton == mainMenuButtons.Count - 1)
+                {
+                    mainMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton = 0;
+                }
+                else
+                {
+                    mainMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton++;
+                }
+
+                menuNextSelectionSound.Play ();
+
+                //bool to make player bring stick back to 0
+                joystick2ReturnToZero = false;
+
+            }
+
+            if (!joystick1ReturnToZero)
+            {
+                if ((Input.GetAxis ("P1Left Stick Vertical") < 0.2f) && Input.GetAxis ("P1Left Stick Vertical") > -0.2f)
+                {
+                    joystick1ReturnToZero = true;
+                }
+            }
+
+            if (!joystick2ReturnToZero)
+            {
+                if ((Input.GetAxis ("P2Left Stick Vertical") < 0.2f) && Input.GetAxis ("P2Left Stick Vertical") > -0.2f)
+                {
+                    joystick2ReturnToZero = true;
+                }
+            }
+
+            //Player Pressed A
+            if (Input.GetButtonDown ("P1A Button") || Input.GetButtonDown ("P2A Button"))
+            {
+                mainMenuButtons[selectedButton].onClick.Invoke ();
+                menuNextSelectionSound.Play ();
+            }
+
+            mainMenuButtons[selectedButton].image.color = Color.blue;
+        }
+
+        else if (optionsMenuActive)
+        {
+            if ((Input.GetAxis ("P1Left Stick Vertical") < -0.8f && joystick1ReturnToZero))
+            {
+                //Change Selection
+                if (selectedButton == 0)
+                {
+                    optionsMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton = optionsMenuButtons.Count - 1;
+                }
+                else
+                {
+                    optionsMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton--;
+                }
+
+                joystick1ReturnToZero = false;
+
+            }
+
+            else if ((Input.GetAxis ("P2Left Stick Vertical") < -0.8f && joystick1ReturnToZero))
+            {
+                //Change Selection
+                if (selectedButton == 0)
+                {
+                    optionsMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton = optionsMenuButtons.Count - 1;
+                }
+                else
+                {
+                    optionsMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton--;
+                }
+
+                joystick1ReturnToZero = false;
+
+            }
+
+            else if ((Input.GetAxis ("P1Left Stick Vertical") > 0.8f) && joystick1ReturnToZero)
+            {
+                if (selectedButton == optionsMenuButtons.Count - 1)
+                {
+                    optionsMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton = 0;
+                }
+                else
+                {
+                    optionsMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton++;
+                }
+
+                //bool to make player bring stick back to 0
+                joystick1ReturnToZero = false;
+
+            }
+            else if (Input.GetAxis ("P2Left Stick Vertical") > 0.8f && joystick2ReturnToZero)
+            {
+                if (selectedButton == optionsMenuButtons.Count - 1)
+                {
+                    optionsMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton = 0;
+                }
+                else
+                {
+                    optionsMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton++;
+                }
+
+                //bool to make player bring stick back to 0
+                joystick2ReturnToZero = false;
+
+            }
+
+            if (!joystick1ReturnToZero)
+            {
+                if ((Input.GetAxis ("P1Left Stick Vertical") < 0.2f) && Input.GetAxis ("P1Left Stick Vertical") > -0.2f)
+                {
+                    joystick1ReturnToZero = true;
+                }
+            }
+
+            if (!joystick2ReturnToZero)
+            {
+                if ((Input.GetAxis ("P2Left Stick Vertical") < 0.2f) && Input.GetAxis ("P2Left Stick Vertical") > -0.2f)
+                {
+                    joystick2ReturnToZero = true;
+                }
+            }
+
+            //Player Pressed A
+            if (Input.GetButtonDown ("P1A Button") || Input.GetButtonDown ("P2A Button"))
+            {
+                optionsMenuButtons[selectedButton].onClick.Invoke ();
+            }
+
+            optionsMenuButtons[selectedButton].image.color = Color.blue;
+        }
+
+        else if (scoresMenuActive)
+        {
+            if ((Input.GetAxis ("P1Left Stick Vertical") < -0.8f && joystick1ReturnToZero))
+            {
+                //Change Selection
+                if (selectedButton == 0)
+                {
+                    scoresMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton = scoresMenuButtons.Count - 1;
+                }
+                else
+                {
+                    scoresMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton--;
+                }
+
+                menuNextSelectionSound.Play ();
+                joystick1ReturnToZero = false;
+
+            }
+
+            else if ((Input.GetAxis ("P2Left Stick Vertical") < -0.8f && joystick1ReturnToZero))
+            {
+                //Change Selection
+                if (selectedButton == 0)
+                {
+                    scoresMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton = scoresMenuButtons.Count - 1;
+                }
+                else
+                {
+                    scoresMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton--;
+                }
+
+                menuNextSelectionSound.Play ();
+                joystick1ReturnToZero = false;
+
+            }
+
+            else if ((Input.GetAxis ("P1Left Stick Vertical") > 0.8f) && joystick1ReturnToZero)
+            {
+                if (selectedButton == scoresMenuButtons.Count - 1)
+                {
+                    scoresMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton = 0;
+                }
+                else
+                {
+                    scoresMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton++;
+                }
+
+                menuNextSelectionSound.Play ();
+
+                //bool to make player bring stick back to 0
+                joystick1ReturnToZero = false;
+
+            }
+            else if (Input.GetAxis ("P2Left Stick Vertical") > 0.8f && joystick2ReturnToZero)
+            {
+                if (selectedButton == scoresMenuButtons.Count - 1)
+                {
+                    scoresMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton = 0;
+                }
+                else
+                {
+                    scoresMenuButtons[selectedButton].image.color = Color.white;
+                    selectedButton++;
+                }
+
+                menuNextSelectionSound.Play ();
+
+                //bool to make player bring stick back to 0
+                joystick2ReturnToZero = false;
+
+            }
+
+            if (!joystick1ReturnToZero)
+            {
+                if ((Input.GetAxis ("P1Left Stick Vertical") < 0.2f) && Input.GetAxis ("P1Left Stick Vertical") > -0.2f)
+                {
+                    joystick1ReturnToZero = true;
+                }
+            }
+
+            if (!joystick2ReturnToZero)
+            {
+                if ((Input.GetAxis ("P2Left Stick Vertical") < 0.2f) && Input.GetAxis ("P2Left Stick Vertical") > -0.2f)
+                {
+                    joystick2ReturnToZero = true;
+                }
+            }
+
+            //Player Pressed A
+            if (Input.GetButtonDown ("P1A Button") || Input.GetButtonDown ("P2A Button"))
+            {
+                scoresMenuButtons[selectedButton].onClick.Invoke ();
+            }
+
+            scoresMenuButtons[selectedButton].image.color = Color.blue;
+        }
+    }
+
+    public void MainMenuStartGame ()
+    {
+        SceneManager.LoadScene ("Main Game Scene", LoadSceneMode.Single);
+        SceneManager.LoadScene ("Main UI Scene", LoadSceneMode.Additive);
+        SceneManager.LoadScene ("Lighting", LoadSceneMode.Additive);
+    }
+
+    public void OptionsSelected ()
+    {
+        menuNextSelectionSound.Play ();
+
+        MainMenuGO.SetActive (false);
+        OptionsMenuGO.SetActive (true);
+        selectedButton = 0;
+        mainMenuActive = false;
+        optionsMenuActive = true;
+    }
+
+    public void ScoresSelected ()
+    {
+        menuNextSelectionSound.Play ();
+
+        MainMenuGO.SetActive (false);
+        ScoresMenuGO.SetActive (true);
+        selectedButton = 0;
+        mainMenuActive = false;
+        scoresMenuActive = true;
+
+    }
+
+    public void BackButton ()
+    {
+        menuNextSelectionSound.Play ();
+
+        ClearButtonColors ();
+
+        OptionsMenuGO.SetActive (false);
+        ScoresMenuGO.SetActive (false);
+        MainMenuGO.SetActive (true);
+
+        selectedButton = 0;
+
+        mainMenuActive = true;
+        scoresMenuActive = false;
+        optionsMenuActive = false;
+    }
+
+    public void QuitGame ()
+    {
+        // Tell GM to Quit Game
+        Application.Quit ();
+    }
+
+    public void ClearButtonColors ()
+    {
+        foreach (Button tempButton in mainMenuButtons)
+        {
+            tempButton.image.color = Color.white;
+        }
+
+        foreach (Button tempButton in optionsMenuButtons)
+        {
+            tempButton.image.color = Color.white;
+        }
+
+        foreach (Button tempButton in scoresMenuButtons)
+        {
+            tempButton.image.color = Color.white;
+        }
+    }
+
+}
